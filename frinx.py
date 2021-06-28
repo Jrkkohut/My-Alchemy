6c49db6d0a5b0ec40ce952368544f122b0285f56
jrkkohut <kohut.jorik@gmail.com>
2021-06-28 12:40:08 +0200

    commit

--- frinx.py
+++ frinx.py
@@ -3,8 +3,9 @@ import os
 import psycopg2
 import psycopg2.extras
 
-with open("data.json", "r", encoding='utf-8') as f:
-    data = json.load(f)
+
+import json
+import psycopg2
 
 conn = psycopg2.connect(
         host = os.getenv("FRINX_HOST", "localhost"),
@@ -12,7 +13,30 @@ conn = psycopg2.connect(
         user= os.getenv("FRINX_USER", "postgres"),
         password=os.getenv("FRINX_PASSWORD", "tutorial")
     )
-cursor=conn.cursor()
-cursor.execute(
-    "CREATE TABLE IF NOT EXISTS frinx (id serial PRIMARY KEY,connection INTEGER,name VARCHAR(255),type VARCHAR(50),port_chanel_id INTEGER,max_frame_size INTEGER) "
-)
+cursor=conn.cursor
+data = []
+with open('data.json') as f:
+    for line in f:
+        data.append(json.loads(line))
+
+fields = [
+    "id" #SERIAL PRIMARY KEY,
+    "connection" #INTEGER,
+    "name" #VARCHAR(255) NOT NULL,
+    "description" #VARCHAR(255),
+    "config" #json,
+    "type" #VARCHAR(50),
+    "infra_type" #VARCHAR(50),
+    "port_channel_id" #INTEGER,
+    "max_frame_size" #INTEGER
+
+]
+for item in data:
+    my_data = [item[field] for field in fields]
+    for i, v in enumerate(my_data):
+        if isinstance(v, dict):
+            my_data[i] = json.dumps(v)
+    insert_query = "INSERT INTO crm VALUES (%s, %s, %s, %s)"
+    cursor.execute(insert_query, tuple(my_data))
+conn.close
+cursor.close
